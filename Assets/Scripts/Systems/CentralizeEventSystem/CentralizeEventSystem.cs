e85515f511d83907643f8ccc08628f8cbb1f5109
Kapnack <unknowrealperson@gmail.com>
2025-09-15 19:01:54 -0300

    Add TryGet Functions

--- Assets/Scripts/Systems/CentralizeEventSystem/CentralizeEventSystem.cs
+++ Assets/Scripts/Systems/CentralizeEventSystem/CentralizeEventSystem.cs
@@ -23,14 +23,53 @@ namespace Systems.CentralizeEventSystem
             return events.TryGetValue(key, out var e) ? e as SimpleEvent : null;
         }
 
+        public bool TryGet(string key, out SimpleEvent gameEvent)
+        {
+            if (!events.TryGetValue(key, out var e))
+            {
+                gameEvent = null;
+                return false;
+            }
+
+            gameEvent = e as SimpleEvent;
+
+            return gameEvent != null;
+        }
+
         public SingleParamEvent<T> Get<T>(string key)
         {
             return events.TryGetValue(key, out var e) ? e as SingleParamEvent<T> : null;
         }
 
+        public bool TryGet<T>(string key, out SingleParamEvent<T> gameEvent)
+        {
+            if (!events.TryGetValue(key, out var e))
+            {
+                gameEvent = null;
+                return false;
+            }
+
+            gameEvent = e as SingleParamEvent<T>;
+
+            return gameEvent != null;
+        }
+
         public ComplexGameEvent<T1, T2, T3> Get<T1, T2, T3>(string key)
         {
             return events.TryGetValue(key, out var e) ? e as ComplexGameEvent<T1, T2, T3> : null;
+        }
+
+        public bool TryGet<T1, T2, T3>(string key, out ComplexGameEvent<T1, T2, T3> gameEvent)
+        {
+            if (!events.TryGetValue(key, out var e))
+            {
+                gameEvent = null;
+                return false;
+            }
+
+            gameEvent = e as ComplexGameEvent<T1, T2, T3>;
+
+            return gameEvent != null;
         }
 
         public void Unregister(string key) => events.Remove(key);
