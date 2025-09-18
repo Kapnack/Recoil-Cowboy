ed87792fc768e9e7326e72ae6b715756b6b1e075
Kapnack <unknowrealperson@gmail.com>
2025-09-18 17:23:50 -0300

    Add & Implement FlickerOnHit.cs

---
+++ Assets/Scripts/Characters/PlayerSRC/FlickerOnHit.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using Systems;
+using Systems.CentralizeEventSystem;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Characters.PlayerSRC
+{
+    public class FlickerOnHit : MonoBehaviour
+    {
+        [SerializeField] private Color hitColor = Color.red;
+        [SerializeField] private Color healColor = Color.green;
+        [SerializeField] private float flashDuration = 0.1f;
+        [SerializeField] private int amountOfFlickers = 3;
+
+        private Material _mat;
+        private Color _originalColor;
+
+        private void Start()
+        {
+            _mat = GetComponent<Renderer>().material;
+            _originalColor = _mat.color;
+
+            ServiceProvider.TryGetService<ICentralizeEventSystem>(out var eventSystem);
+
+            eventSystem.Get<int, int, int>(PlayerEventKeys.LivesChange).AddListener(Flash);
+        }
+
+        private void Flash(int previous, int current, int max)
+        {
+            if (current == previous)
+                return;
+
+            StartCoroutine(FlashRoutine(current < previous ? hitColor : healColor));
+        }
+
+        private IEnumerator FlashRoutine(Color color)
+        {
+            for (var i = 0; i < amountOfFlickers; i++)
+            {
+                _mat.color = color;
+                yield return new WaitForSeconds(flashDuration);
+                _mat.color = _originalColor;
+                yield return new WaitForSeconds(flashDuration);
+            }
+        }
+    }
+}
