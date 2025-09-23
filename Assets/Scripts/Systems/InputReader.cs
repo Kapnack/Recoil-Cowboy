f7f96d5ec01b6d16e8c08ef30ba5b02db5edcaed
Kapnack <unknowrealperson@gmail.com>
2025-09-23 20:52:40 -0300

    Update InputReader.cs
    Add Instant Reload input handeler

--- Assets/Scripts/Systems/InputReader.cs
+++ Assets/Scripts/Systems/InputReader.cs
@@ -11,10 +11,11 @@ namespace Systems
 
         [SerializeField] private InputActionReference attack;
         [SerializeField] private InputActionReference reload;
+        [SerializeField] private InputActionReference activeInstantReload;
 
         private readonly SimpleEvent _attackEvent = new();
-        
         private readonly SimpleEvent _reloadEvent = new();
+        private readonly SimpleEvent _instantReloadEvent = new();
 
         private IEnumerator Start()
         {
@@ -25,30 +26,31 @@ namespace Systems
 
             _eventSystem.Register(PlayerEventKeys.Attack, _attackEvent);
             _eventSystem.Register(PlayerEventKeys.ReloadOvertime, _reloadEvent);
+            _eventSystem.Register(PlayerEventKeys.InstantReload, _instantReloadEvent);
         }
 
         private void OnEnable()
         {
             attack.action.started += HandleAttack;
             reload.action.started += HandleReload;
+            activeInstantReload.action.started += HandleInstantReload;
         }
 
         private void OnDisable()
         {
             attack.action.started -= HandleAttack;
             reload.action.started -= HandleReload;
+            activeInstantReload.action.started -= HandleInstantReload;
         }
 
         #region PlayerActionsEvents
-        private void HandleAttack(InputAction.CallbackContext _)
-        {
-            _attackEvent?.Invoke();
-        }
-
-        private void HandleReload(InputAction.CallbackContext _)
-        {
-            _reloadEvent?.Invoke();
-        }
+        
+        private void HandleAttack(InputAction.CallbackContext _) => _attackEvent?.Invoke();
+        
+        private void HandleReload(InputAction.CallbackContext _) => _reloadEvent?.Invoke();
+        
+        private void  HandleInstantReload(InputAction.CallbackContext _) =>  _instantReloadEvent?.Invoke();
+        
         #endregion
     }
 }
