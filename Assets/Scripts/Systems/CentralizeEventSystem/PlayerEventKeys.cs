5ff6e4a62f5fba4281efc1f8e26845c6b0c795b1
Kapnack <unknowrealperson@gmail.com>
2025-09-23 20:51:05 -0300

    Update PlayerEventKeys.cs
    Add Instant Reload Event key

--- Assets/Scripts/Systems/CentralizeEventSystem/PlayerEventKeys.cs
+++ Assets/Scripts/Systems/CentralizeEventSystem/PlayerEventKeys.cs
@@ -7,7 +7,8 @@
         public const string BulletsChange = "BULLETS_CHANGE";
         public const string Dies = "DIES";
         public const string Wins = "WINS";
-        public const string Reload =  "RELOAD";
-        public const string ReloadOvertime =  "RELOAD_OVERTIME";
+        public const string Reload = "RELOAD";
+        public const string ReloadOvertime = "RELOAD_OVERTIME";
+        public const string InstantReload = "INSTANT_RELOAD";
     }
 }
