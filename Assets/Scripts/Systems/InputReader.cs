f8e75e7744210fdc2a4f79e7c515972a4b98a9c3
Kapnack <unknowrealperson@gmail.com>
2025-09-23 19:44:54 -0300

    Add Reload input
    Add Reload input into InputReader

--- Assets/Scripts/Systems/InputReader.cs
+++ Assets/Scripts/Systems/InputReader.cs
@@ -10,8 +10,11 @@ namespace Systems
         private ICentralizeEventSystem _eventSystem;
 
         [SerializeField] private InputActionReference attack;
+        [SerializeField] private InputActionReference reload;
 
         private readonly SimpleEvent _attackEvent = new();
+        
+        private readonly SimpleEvent _reloadEvent = new();
 
         private IEnumerator Start()
         {
@@ -21,21 +24,31 @@ namespace Systems
             }
 
             _eventSystem.Register(PlayerEventKeys.Attack, _attackEvent);
+            _eventSystem.Register(PlayerEventKeys.Reload, _reloadEvent);
         }
 
         private void OnEnable()
         {
             attack.action.started += HandleAttack;
+            reload.action.started += HandleReload;
         }
 
         private void OnDisable()
         {
             attack.action.started -= HandleAttack;
+            reload.action.started -= HandleReload;
         }
 
+        #region PlayerActionsEvents
         private void HandleAttack(InputAction.CallbackContext _)
         {
             _attackEvent?.Invoke();
         }
+
+        private void HandleReload(InputAction.CallbackContext _)
+        {
+            _reloadEvent?.Invoke();
+        }
+        #endregion
     }
 }
