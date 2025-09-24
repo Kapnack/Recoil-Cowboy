0de0266b2618098db0d9c742fc1cc1c608274341
Kapnack <unknowrealperson@gmail.com>
2025-09-23 21:14:23 -0300

    Add VercionDisplay.cs

---
+++ Assets/Scripts/VersionDisplay.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class VersionDisplay : MonoBehaviour
+{
+    [SerializeField] private GameObject versionCanvas;
+
+    private TMP_Text _version;
+    private string _versionFormat;
+
+    private void Awake()
+    {
+        _version = versionCanvas.GetComponent<TMP_Text>();
+        _versionFormat = _version.text;
+        _version.text = string.Format(_versionFormat, 0);
+    }
+
+    private void Start()
+    {
+        _version.text = string.Format(_versionFormat, Application.version);
+    }
+}
