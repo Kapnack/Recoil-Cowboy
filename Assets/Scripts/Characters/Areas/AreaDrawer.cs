5991b24e087b62525e0628ab94cc8ea83592eda3
Kapnack <unknowrealperson@gmail.com>
2025-10-21 12:59:28 -0300

    Add AreaDrawer.cs
    This is an Abstract Base Class for the other AreaDrawers

---
+++ Assets/Scripts/Characters/Areas/AreaDrawer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Characters.Areas
+{
+    [Serializable]
+    [RequireComponent(typeof(LineRenderer))]
+    public abstract class AreaDrawer : MonoBehaviour
+    {
+        [Tooltip("Number of segments (higher = smoother arc)")] [SerializeField]
+        protected int resolution = 30;
+
+        protected LineRenderer LineRenderer;
+
+        protected void Awake()
+        {
+            LineRenderer = GetComponent<LineRenderer>();
+            LineRenderer.useWorldSpace = true;
+            LineRenderer.loop = true;
+            LineRenderer.startWidth = 0.1f;
+            LineRenderer.endWidth = 0.1f;
+            LineRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        }
+
+        private void LateUpdate() => SetArea();
+        
+        protected abstract void SetArea();
+    }
+}
