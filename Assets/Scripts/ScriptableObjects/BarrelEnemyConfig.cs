d42dbf3792a34f145ce5200f7a839c10e4adbfc9
Kapnack <unknowrealperson@gmail.com>
2025-10-21 01:05:31 -0300

    Update BarrelEnemyConfig.cs
    Change variables names for an easier understanding of there use

--- Assets/Scripts/ScriptableObjects/BarrelEnemyConfig.cs
+++ Assets/Scripts/ScriptableObjects/BarrelEnemyConfig.cs
@@ -1,16 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ScriptableObjects
 {
     [CreateAssetMenu(fileName = "BarrelEnemyConfig", menuName = "ScriptableObjects/BarrelEnemyConfig")]
     public class BarrelEnemyConfig : ScriptableObject
     {
-        [field: SerializeField] public float AreaOfSight  { get; private set; }
-       [field: SerializeField] public float AttackRadius { get; private set; }
-       [field: SerializeField] public float FireOffset { get; private set; }
-       [field: SerializeField] public float FireForce { get; private set; }
-       [field: SerializeField] public float ColdDown { get; private set; }
-       [field: SerializeField] public float RaycastOffSet { get; private set; }
-       [field: SerializeField] public float RaycastDistance { get; private set; }
+        [field: SerializeField] public float HideDistance { get; private set; }
+        [field: SerializeField] public float AttackDistance { get; private set; }
+        [field: SerializeField] public float AttackRadius { get; private set; }
+        [field: SerializeField] public float FireOffset { get; private set; }
+        [field: SerializeField] public float FireForce { get; private set; }
+        [field: SerializeField] public float ColdDown { get; private set; }
+        [field: SerializeField] public float RaycastOffSet { get; private set; }
     }
 }
