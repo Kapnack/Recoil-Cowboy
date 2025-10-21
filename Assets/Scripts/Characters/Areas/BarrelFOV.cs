6fcd5dc92b2bc1964bc670cf918f27517bb4e29a
Kapnack <unknowrealperson@gmail.com>
2025-10-21 13:00:49 -0300

    Add BarrelFOV.cs
    This draws the area where the Barrel Enemy can see the player

---
+++ Assets/Scripts/Characters/Areas/BarrelFOV.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using ScriptableObjects;
+using UnityEngine;
+
+namespace Characters.Areas
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class BarrelFOV : AreaDrawer
+    {
+        [SerializeField] private BarrelEnemyConfig config;
+
+        private readonly List<Vector3> _points = new();
+
+        protected override void SetArea()
+        {
+            _points.Clear();
+        
+            var origin = transform.position + transform.right * config.RaycastOffSet;
+            _points.Add(origin);
+
+            for (int i = 0; i <= resolution; i++)
+            {
+                var angle = Mathf.Lerp(-config.AttackRadius, config.AttackRadius, i / (float)resolution);
+            
+                var dir = Quaternion.Euler(0, 0, angle) * transform.right;
+            
+                var point = origin + dir * config.AttackDistance;
+
+                _points.Add(point);
+            }
+            
+            LineRenderer.positionCount = _points.Count;
+            LineRenderer.SetPositions(_points.ToArray());
+        }
+    }
+}
