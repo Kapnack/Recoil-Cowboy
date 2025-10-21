221cfc39e3d9c36910ef79612e570a52718b593c
Kapnack <unknowrealperson@gmail.com>
2025-10-21 12:56:42 -0300

    Add CharacterConfig
    CharacterConfig will be used to draw the area of the characters

---
+++ Assets/Scripts/ScriptableObjects/CharacterConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace ScriptableObjects
+{
+    [Serializable]
+    public class CharacterConfig : ScriptableObject
+    {
+        [field: SerializeField] public float AreaOfSight { get; set; }
+    }
+}
