cfcd776a5df34aaf1a767e398d07f6483aabc647
Kapnack <unknowrealperson@gmail.com>
2025-10-29 00:56:06 -0300

    Fix Bugs #2

--- Assets/Scripts/Shaders/ShaderSettings.cs
+++ Assets/Scripts/Shaders/ShaderSettings.cs
@@ -9,16 +9,20 @@ namespace Shaders
         private IShaderSettings settings;
 
         private Toggle toggle;
-    
+
         private void Awake()
         {
+            Cursor.visible = true;
+
             settings = ServiceProvider.GetService<IShaderSettings>();
-        
+
             toggle = GetComponentInChildren<Toggle>();
-        
+
             toggle.isOn = settings.GetFlickerValue() != 0;
             toggle.onValueChanged.AddListener(ActiveShaderFlicker);
         }
+
+        private void OnDestroy() => Cursor.visible = false;
 
         private void ActiveShaderFlicker(bool active) => settings?.SetFlickerActive(active);
 
