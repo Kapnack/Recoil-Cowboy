f42888ab34910e1806244d92141feb73249bae7f
Kapnack <unknowrealperson@gmail.com>
2025-11-24 06:12:57 -0300

    Fix 2nd part of the cause of level multiplication

--- Assets/Scripts/Menus/WinMenuManager.cs
+++ Assets/Scripts/Menus/WinMenuManager.cs
@@ -20,22 +20,28 @@ namespace Menus
         [SerializeField] private Button mainMenuButton;
 
         private CentralizeEventSystem _eventSystem;
-        
+
         private void Awake()
         {
             _eventSystem = ServiceProvider.GetService<CentralizeEventSystem>();
-            
+
             nextLevelButton.onClick.AddListener(OnNextLevel);
             mainMenuButton.onClick.AddListener(OnMainMenu);
-            
+
             _pointsTextFormat = pointsText.text;
             _distanceTextFormat = disntanceText.text;
             pointsText.text = string.Format(_pointsTextFormat, stats.CurrentKillPoints, stats.RecordKillPoints);
             disntanceText.text = string.Format(_distanceTextFormat, stats.CurrentDistance, stats.RecordDistance);
         }
 
-        private void OnNextLevel() => _eventSystem.Get<LoadGameplay>()?.Invoke();
+        private void OnNextLevel()
+        {
+            _eventSystem.Get<LoadGameplay>()?.Invoke();
+        }
 
-        private void OnMainMenu() => _eventSystem.Get<LoadMainMenu>()?.Invoke();
+        private void OnMainMenu()
+        {
+            _eventSystem.Get<LoadMainMenu>()?.Invoke();
+        }
     }
 }
