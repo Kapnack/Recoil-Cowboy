989645db594517df2b7e9e6f982cc72ba448ff57
Kapnack <unknowrealperson@gmail.com>
2025-11-24 23:59:58 -0300

    Add change to main menu manager

--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -1,26 +1,38 @@
 using Systems;
 using Systems.CentralizeEventSystem;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject settingsMenu;
     [SerializeField] private GameObject creditsMenu;
-
+    
+    [Header("Buttons")]
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button settingsButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button exitButton;
+    
     private CentralizeEventSystem _eventSystem;
     
     private void Awake()
     {
         _eventSystem = ServiceProvider.GetService<CentralizeEventSystem>();
+        
+        playButton.onClick.AddListener(OnGame);
+        settingsButton.onClick.AddListener(OnSettings);
+        creditsButton.onClick.AddListener(OnCredits);
+        exitButton.onClick.AddListener(OnExitGame);
     }
 
-    public void OnNextLevel() => _eventSystem.Get<LoadGameplay>()?.Invoke();
+    private void OnGame() => _eventSystem.Get<LoadGameplay>()?.Invoke();
 
-    public void OnSettings() => Instantiate(settingsMenu);
+    private void OnSettings() => Instantiate(settingsMenu);
     
-    public void OnCredits() => Instantiate(creditsMenu);
+    private void OnCredits() => Instantiate(creditsMenu);
 
-    public void OnExitGame()
+    private void OnExitGame()
     {
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
