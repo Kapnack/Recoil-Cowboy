a4a3b80b2e531c261306cdfc87164fbf30aac561
Kapnack <unknowrealperson@gmail.com>
2025-11-27 12:12:24 -0300

    Update ButtonHoverDetector.cs

--- Assets/ButtonHoverDetector.cs
+++ Assets/ButtonHoverDetector.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -5,12 +6,28 @@ using UnityEngine.UI;
 public class ButtonHoverDetector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private Button _creditsButton;
+    private TMP_Text _text;
+    private Color originalTextColor;
+
+    [SerializeField] private Color buttonHoverColor = new Color(0.65f, 0.65f, 0.65f, 1);
+    [SerializeField] private Color textHoverColor = new Color(0.7f, 0.7f, 0.7f, 1);
 
     private void Awake()
     {
         _creditsButton = gameObject.GetComponent<Button>();
+        _text = gameObject.GetComponentInChildren<TMP_Text>();
+        originalTextColor = _text.color;
     }
-    
-    public void OnPointerEnter(PointerEventData eventData) => _creditsButton.image.color = new Color(0.65f, 0.65f, 0.65f, 255);
-    public void OnPointerExit(PointerEventData eventData) => _creditsButton.image.color = Color.white;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _creditsButton.image.color = buttonHoverColor;
+        _text.color = textHoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _creditsButton.image.color = Color.white;
+        _text.color = originalTextColor;
+    }
 }
